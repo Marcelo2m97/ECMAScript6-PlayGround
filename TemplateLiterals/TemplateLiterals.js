50cd9825f2fc04a8049b2092e60da5350abca678
Marcelo Martínez <marcelo2m1097@gmail.com>
2019-12-18 20:11:59 -0600

    Example inserting HTML code using template literals.

--- TemplateLiterals/TemplateLiterals.js
+++ TemplateLiterals/TemplateLiterals.js
@@ -4,9 +4,25 @@ const name = "Marcelo";
 const lastName = "Martínez";
 const age = 22;
 
-//Old way
+//Without template literals
 const phrase1 = "My full name is " + name + " " + lastName + " and I'm " + age + "years old.";
 console.log(phrase1);
-//New way with template literals
+//With template literals
 const phrase2 = `My full name is ${name} ${lastName} and I'm ${age} years old.`;
 console.log(phrase2);
+
+//Without template literals
+const withOutTLDiv = document.createElement("div");
+withOutTLDiv.innerHTML = "<p>" + name + "</p>" + "<p>" + lastName + "</p>";
+//With template literals
+const withTLDiv = document.createElement("div");
+withTLDiv.innerHTML = `
+    <h1>My name is: ${name}</h1>
+    <h3>My lastname is: ${lastName}</h3>
+    <h6>My age is: ${age}</h6>
+    <p>Template literals are so cool!</p>
+`;
+
+//Adding divs on the HTML
+document.body.appendChild(withOutTLDiv);
+document.body.appendChild(withTLDiv);
