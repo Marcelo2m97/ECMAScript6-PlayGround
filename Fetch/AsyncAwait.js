45096167c006264c3ae15cd0af602e721fc8bf12
Marcelo Martínez <marcelo2m1097@gmail.com>
2020-06-29 21:28:21 -0600

    Reusing examples to use map for arrays.

--- Fetch/AsyncAwait.js
+++ Fetch/AsyncAwait.js
@@ -1,11 +1,11 @@
 //How to use async and await
+//Filling array with map
 const obtenerPokemones = async() => {
     try{
         const response = await fetch('https://pokeapi.co/api/v2/pokemon')
         const data = await response.json();
-        data.results.forEach(element => {
-            console.log(element.name)
-        });
+        let arrayNombres2 = data.results.map(pokemon => pokemon.name)
+        console.log(arrayNombres2)
     } catch(error) {
         console.log(error)
     }
--- Fetch/Fetch.js
+++ Fetch/Fetch.js
@@ -1,10 +1,14 @@
 //How to use fetch
+//Filling array without map
 fetch('https://pokeapi.co/api/v2/pokemon')
     .then(response => response.json())
     .then(data => {
         console.log(data.results)
+        let arrayNombres = []
         data.results.forEach(element => {
+            arrayNombres.push(element.name)
             console.log(element.name)
         });
+        console.log(arrayNombres)
     })
     .catch(error => console.log(error))
