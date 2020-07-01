0d0b0b857a9327c0b2a938c9cc32a3aeed8cd4a4
Marcelo Martínez <marcelo2m1097@gmail.com>
2020-07-01 11:39:17 -0600

    Basic example of the use of filter.

--- Fetch/AsyncAwait.js
+++ Fetch/AsyncAwait.js
@@ -1,11 +1,14 @@
 //How to use async and await
 //Filling array with map
+//Using filter
 const obtenerPokemones = async() => {
     try{
         const response = await fetch('https://pokeapi.co/api/v2/pokemon')
         const data = await response.json();
         let arrayNombres2 = data.results.map(pokemon => pokemon.name)
+        let arrayNombres3 = data.results.filter(pokemon => pokemon.name !== 'bulbasaur')
         console.log(arrayNombres2)
+        console.log(arrayNombres3)
     } catch(error) {
         console.log(error)
     }
